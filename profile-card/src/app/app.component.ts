92d57a7f84241aa81eadccccce15d496d7a2eedb
stefanborcia <stefigabi89@yahoo.com>
2025-04-16 13:47:17 +0200

    Implement input & data binding

--- profile-card/src/app/app.component.ts
+++ profile-card/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
